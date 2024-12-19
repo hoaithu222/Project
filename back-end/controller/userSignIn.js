b50f3a2b149a7ca22d3746f5859147285bf38958
Thu222 <thu601925@gmail.com>
2024-12-19 12:21:55 +0700

    Nộp bài tập

--- back-end/controller/userSignIn.js
+++ back-end/controller/userSignIn.js
@@ -24,6 +24,7 @@ async function userSignInController(req, res) {
             const tokenOption = {
                 httpOnly: true,
                 secure: true,
+                sameSite: "None",
             }
             res.cookie("token", token, tokenOption).json({
                 message: "Login Successfully",
