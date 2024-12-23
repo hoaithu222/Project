f92bb6e33b3e3e6165120a005cf7e360f8879597
Thu222 <thu601925@gmail.com>
2024-12-23 19:42:56 +0700

    add

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -4,6 +4,7 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import router from "./routers/index.jsx";
+
 import { store } from "./store/store.js";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
